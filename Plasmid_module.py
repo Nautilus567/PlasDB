d4703ad149166dbf84f92af704ac6660ed84fc91
Erwin Quiroga <erquiroga@live.cl>
2017-05-09 00:01:28 -0300

    Alpha Status 0.0.1

---
+++ Plasmid_module.py
@@ -0,0 +1,16 @@
+"""
+PlasDB Plasmid Module:
+
+This file includes the plasmid class and several functions to keep track of annotations ans insertions
+
+"""
+
+
+class Plasmid:
+    def __init__(self, name, id, insert, resistance, sequence):
+        self.name = name
+        self.id = id
+        self.insert = insert
+        self.resistance = resistance
+        self.sequence = sequence
+    def
---
+++ README.md
@@ -0,0 +1,5 @@
+# PlasDB
+## A simple plasmid database management
+
+*Still on construction*
+
