0e1977ddc498ee7cdd970d4e1f082b71254ad7d5
Erwin Quiroga <erquiroga@live.cl>
2017-05-10 17:22:48 -0300

    Creating self test statement

--- Plasmid_module.py
+++ Plasmid_module.py
@@ -4,7 +4,7 @@ PlasDB Plasmid Module:
 This file includes the plasmid class and several functions to keep track of annotations ans insertions
 
 """
-
+#import os   ##For further testing
 
 class Plasmid:
     def __init__(self, name, id, insert, resistance, sequence):
@@ -17,9 +17,21 @@ class Plasmid:
         return "Plasmid {0}".format(self.name)
     def __str__(self):
         if len(self.sequence) > 50:
-            seq = self.sequence[:20] + "[...]" + self.sequence[-21:-1]
+            seq = self.sequence[:20] + "[...]" + self.sequence[-21:]
         else:
             seq = self.sequence
-        return "Plasmid entry: \nName: {0} \nId: {1} \nInsert: {2} \n Antibiotic Resistance: {3} \nSequence: {4}\n ".format(self.name, self.id, self.insert, self.resistance, seq)
+        return "Plasmid entry: \nName: {0} \nID: {1} \nInsert: {2} \nAntibiotic Resistance: {3} \nSequence: {4}\n ".format(self.name, self.id, self.insert, self.resistance, seq)
     def restrictioncuts(self, Enzimes):
         pass
+
+
+
+
+
+
+#Test of working
+
+Plas = Plasmid('Test Plasmid', 'pTest','NA' , 'Amp','ggggaattgtgagcggataacaattcccctctagaaataattttgtttaactttaagaaggagatataccatgggcagca')
+
+print(Plas)
+
