c85bc3233d30347a9ad826cb94f617a0cee2c4bc
Erwin Quiroga <erquiroga@live.cl>
2017-05-11 16:10:40 -0300

    New Dependency Biopython

--- Plasmid_module.py
+++ Plasmid_module.py
@@ -6,13 +6,23 @@ This file includes the plasmid class and several functions to keep track of anno
 """
 #import os   ##For further testing
 
+from Bio.Seq import Seq
+from Bio.Alphabet import generic_dna
+
 class Plasmid:
+    """
+    Plasmid object class
+    """
     def __init__(self, name, id, insert, resistance, sequence):
+
         self.name = name
         self.id = id
         self.insert = insert
         self.resistance = resistance
-        self.sequence = sequence
+        if type(sequence) == Seq:
+            self.sequence = sequence
+        elif type(sequence) == str:
+            self.sequence = Seq(sequence, generic_dna)
     def __repr__(self):
         return "Plasmid {0}".format(self.name)
     def __str__(self):
@@ -21,8 +31,10 @@ class Plasmid:
         else:
             seq = self.sequence
         return "Plasmid entry: \nName: {0} \nID: {1} \nInsert: {2} \nAntibiotic Resistance: {3} \nSequence: {4}\n ".format(self.name, self.id, self.insert, self.resistance, seq)
-    def restrictioncuts(self, Enzimes):
-        pass
+    def restriction_cuts(self, Enzimes):
+        query = str(self.sequence)
+
+
 
 
 
